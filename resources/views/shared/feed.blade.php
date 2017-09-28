068bb228c44efcc4f08f90fa79f091d1f6b7cc01
fthvg <fthvgb1@163.com>
2017-09-28 22:28:16 +0800

    首页微博列表数量小修

--- resources/views/shared/feed.blade.php
+++ resources/views/shared/feed.blade.php
@@ -1,4 +1,4 @@
-@if(count($feed_items)>1)
+@if(count($feed_items))
     <ol class="statuses">
         @foreach($feed_items as $feed_item)
             @include('status.status',['user'=>$feed_item->user,'status'=>$feed_item])
